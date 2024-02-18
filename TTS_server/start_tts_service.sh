f8d5f70ea2b7766b9d1f3cc8b0618cad3601cce7
Elbios <elbiosgh@gmail.com>
2024-02-18 05:18:06 +0100

    Fix xtts-webui gradio by switching to cuda 11.8 in TTS docker

--- TTS_server/start_tts_service.sh
+++ TTS_server/start_tts_service.sh
@@ -5,7 +5,7 @@ cd /xtts_app
 #cd XTTS-RVC-UI
 cd xtts-webui
 source venv/bin/activate
-python app.py --deepspeed --share
+python app.py --deepspeed --share --host 0.0.0.0 --port 8010
 #python app.py > /home/debian/TTS_server/xtts_rvc_ui_log.txt 2>&1
 
 # Checking if INCLUDE_TTS is set to true and starting xTTS server
